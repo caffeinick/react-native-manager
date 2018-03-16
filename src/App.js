d3cd7aea35a9904a0262a0f64abb0cbc22a25f00
Juman Pak <qkrwnaks92@gmail.com>
2018-03-16 11:17:44 +0900

    119. Redux Thunk in Practice

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { Provider } from 'react-redux';
-import { createStore } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
+import ReduxThunk from 'redux-thunk';
 import firebase from 'firebase';
 
 import config from './bin/config';
@@ -13,8 +14,12 @@ class App extends Component {
   }
   
   render() {
+    const store = createStore(
+      reducers, {}, applyMiddleware(ReduxThunk)
+    );
+
     return (
-      <Provider store={createStore(reducers)}>
+      <Provider store={store}>
         <LoginForm />
       </Provider>
     );
