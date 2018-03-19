8738d9881184fc270023682b8afdac23023cc7f0
Juman Pak <qkrwnaks92@gmail.com>
2018-03-19 17:18:08 +0900

    131. Navigation Bar Buttons

--- src/screens/EmployeeScreen.js
+++ src/screens/EmployeeScreen.js
@@ -1,10 +1,20 @@
 import React, { Component } from 'react';
-import { SafeAreaView } from 'react-native';
+import { SafeAreaView, Button } from 'react-native';
 import { withNavigation } from 'react-navigation';
 
 import EmployeeList from '../components/EmployeeList';
 
 class EmployeeScreen extends Component {
+  static navigationOptions = {
+    headerRight: (
+      <Button
+        onPress={() => { console.log(`right!!!`) }}
+        title='info'
+        color='#fff'
+      />
+    )
+  };
+
   render() {
     return (
       <SafeAreaView style={{ flex: 1 }}>
