b23f0aada3f2f1bad2f2c36ae25cb4bb00952d3c
Juman Pak <qkrwnaks92@gmail.com>
2018-03-20 13:35:57 +0900

    145. Fetching Data from Firebase

--- src/actions/EmployeeActions.js
+++ src/actions/EmployeeActions.js
@@ -21,3 +21,14 @@ export const employeeCreate = ({ name, phone, shift, navigation }) => {
       });
   };
 };
+
+export const employeesFetch = () => {
+  const { currentUser } = firebase.auth();
+
+  return (dispatch) => {
+    firebase.database().ref(`/users/${currentUser.uid}/employees`)
+      .on('value', snapshot => {
+        dispatch({ type: 'employees_fetch_success', payload: snapshot.val() });
+      });
+  };
+};
